0b2795170f49b22b4b969e8b10d9c5df7b719cb3
monicamarshall <marshallmonica@yahoo.com>
2020-10-23 09:23:52 -0600

    Add messages to the board via the app

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,24 @@
 from django.views.generic import ListView
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import render
 
 
 class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
     context_object_name = 'all_posts_list'
+    
+
+
+def home(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)        
+        if form.is_valid():
+            post = Post(
+                text=form.cleaned_data["body"]
+            )
+            post.save()
+    all_posts_list = Post.objects.all()
+    context = {"all_posts_list": all_posts_list,"form": PostForm()}
+    return render(request, "home.html", context)
